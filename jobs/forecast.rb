261c398dd6f97347ffd48282d62ba9d7e7b7d2ba
Lilium JSN <karapatisjim@yahoo.gr>
2016-06-19 14:02:02 +0300

    Update forecast.rb

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -15,10 +15,10 @@ forecast_location_long = "23.726789"
 forecast_units = "si"
   
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  https = Net::HTTPS.new("api.forecast.io", 443)
+  https = Net::HTTP.new("https://api.forecast.io", 443)
   https.use_ssl = true
   https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  response = https.request(Net::HTTPS::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
+  response = https.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
   forecast = JSON.parse(response.body)  
   forecast_current_temp = forecast["currently"]["temperature"].round
   forecast_hour_summary = forecast["minutely"]["summary"]
