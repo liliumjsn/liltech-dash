af60adc3d21c0aec8df488cb8e0b1a481bf7c903
Lilium JSN <karapatisjim@yahoo.gr>
2016-06-20 15:57:02 +0300

    Update currencies.rb

--- jobs/currencies.rb
+++ jobs/currencies.rb
@@ -2,11 +2,11 @@ require 'iso4217'
 
 ISO4217::Currency.base_currency = 'EUR'
 
-usd_currency = ISO4217::Currency.from_code('USD')
-gbp_currency = ISO4217::Currency.from_code('GBP')
 
-SCHEDULER.every '5m', :first_in => 0 do
 
+SCHEDULER.every '60m', :first_in => 0 do
+  usd_currency = ISO4217::Currency.from_code('USD')
+  gbp_currency = ISO4217::Currency.from_code('GBP')
   
   send_event('usd_cur',   { current: usd_currency.exchange_rate })
   send_event('gbp_cur',   { current: gbp_currency.exchange_rate })
