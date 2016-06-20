227ea92ba2b457fa7421286503cc3bbf18b5ace8
Lilium JSN <karapatisjim@yahoo.gr>
2016-06-20 20:27:55 +0300

    Update facebook.rb

--- jobs/facebook.rb
+++ jobs/facebook.rb
@@ -8,7 +8,7 @@ access_token = '593668544144964|i2xVZ4G78mZROfPX5NzeyrjetxA'
 #https://graph.facebook.com/142285229525207?access_token=593668544144964|i2xVZ4G78mZROfPX5NzeyrjetxA&fields=fan_count
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  uri = URI('https://graph.facebook.com/142285229525207?access_token=593668544144964|i2xVZ4G78mZROfPX5NzeyrjetxA&fields=fan_count')
+  uri = URI('https://graph.facebook.com/#{facebook_id}?access_token=#{access_token}=fan_count')
   response = Net::HTTP.get_response(uri)
   data = JSON.parse(response.body)
   send_event('facebook_likes', current: data['fan_count'])
