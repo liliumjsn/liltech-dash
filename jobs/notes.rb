a5560fb4d61173f43b8083b4d907fee7f7491791
Lilium JSN <karapatisjim@yahoo.gr>
2016-06-21 01:49:08 +0300

    Update notes.rb

--- jobs/notes.rb
+++ jobs/notes.rb
@@ -2,4 +2,5 @@ SCHEDULER.every '5s', :first_in => 0 do
   send_event('notes',  { note1: "test1" })
   send_event('notes',  { note2: "test2" })
   send_event('notes',  { note3: "test3" })
+  send_event('notes',  { note4: "test4" })
 end
