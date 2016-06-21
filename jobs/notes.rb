415521cda6bc53b9f547f49258437691207d7b44
Lilium JSN <karapatisjim@yahoo.gr>
2016-06-21 03:57:06 +0300

    Update notes.rb

--- jobs/notes.rb
+++ jobs/notes.rb
@@ -3,11 +3,11 @@ require 'json'
 
 SCHEDULER.every '10m', :first_in => 0 do 
     
-    uri = URI('https://www.dropbox.com/s/3wbu1d34ey346st/liltech-dash-notes.txt')
+    uri = URI('https://dl.dropboxusercontent.com/s/3wbu1d34ey346st/liltech-dash-notes.txt')
     response = Net::HTTP.get_response(uri)
     data = JSON.parse(response.body)
 
-    send_event "notes", {   note1: "test1", 
+    send_event "notes", {   note1: data['note1'], 
                             note2: "test2", 
                             note3: "test3", 
                             note4: "test4", 
